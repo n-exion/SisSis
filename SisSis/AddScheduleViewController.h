761acb7224bac90139b8a563b5287cd40e5552aa
Oiki Tomoaki <laket72@gmail.com>
2012-01-21 00:59:41 +0900

    出発時刻の追加ボタンを追加

--- SisSis/AddScheduleViewController.h
+++ SisSis/AddScheduleViewController.h
@@ -10,20 +10,23 @@
 #import "EditableCell.h"
 #import "DoubleRowCell.h"
 #import "WorkTimeDecideViewController.h"
+#import "StartTimeDecideViewController.h"
 
 //EventKit
 @interface AddScheduleViewController : UITableViewController{
-    EditableCell* editableCell;
-    WorkTimeDecideViewController* workTimeDecideController;
-    
-    NSDate* startTime;
-    NSDate* endTime;
+  EditableCell* editableCell;
+  WorkTimeDecideViewController* workTimeDecideController;
+  StartTimeDecideViewController* startTimeDecideController;
+  
+  NSDate* startTime;
+  NSDate* endTime;
 }
 
 @property (strong, nonatomic) IBOutlet EditableCell* editableCell;
 @property (strong, nonatomic) IBOutlet DoubleRowCell* doubleRowCell;
 
 @property (strong) WorkTimeDecideViewController* workTimeDecideController;
+@property (strong) StartTimeDecideViewController* startTimeDecideController;
 @property (strong) NSDate* startTime;
 @property (strong) NSDate* endTime;
 
