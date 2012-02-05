e624f166ec8451bda14b7db2ede3ad89efb5b773
Oiki Tomoaki <laket72@gmail.com>
2012-02-05 19:24:28 +0900

    ButtonEditableCellwo

---
+++ SisSis/ButtonEditableCell.h
@@ -0,0 +1,15 @@
+//
+//  EditableCell.h
+//  SampleTableView2
+//
+//  Created by じょん たいたー on 12/01/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ButtonEditableCell : UITableViewCell
+
+@property (retain, nonatomic) IBOutlet UIButton *addButton;
+@property (retain, nonatomic) IBOutlet UITextField *inputField;
+@end
---
+++ SisSis/ButtonEditableCell.m
@@ -0,0 +1,45 @@
+//
+//  EditableCell.m
+//  SampleTableView2
+//
+//  Created by じょん たいたー on 12/01/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ButtonEditableCell.h"
+
+@implementation ButtonEditableCell
+@synthesize addButton;
+@synthesize inputField;
+
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)dealloc {
+    [inputField release];
+  [addButton release];
+    [super dealloc];
+}
+@end
